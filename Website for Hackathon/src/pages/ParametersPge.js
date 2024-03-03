f9a8c26b3c20c78a1861cd8348863ca240aa37c7
Christopher M <christopher_mao@branson.org>
2024-03-02 17:37:28 -0800

    stuff

--- Website for Hackathon/src/pages/ParametersPge.js
+++ Website for Hackathon/src/pages/ParametersPge.js
@@ -70,17 +70,17 @@ const ParametersPge = () => {
                 onClick={onButtonMenu1Click}
               >
                 <img
-                  className={styles.handPointFinger}
+                  className={styles.mediaCirclesPlayCircles}
                   alt=""
                   src="/hand-point-finger.svg"
                 />
-                <div className={styles.account1}>Choose Eviroment</div>
+                <div className={styles.account}>Choose Eviroment</div>
                 <img
                   className={styles.handPointFinger1}
                   alt=""
                   src="/hand-point-finger.svg"
                 />
-                <div className={styles.glow2} />
+                <div className={styles.glow1} />
               </button>
               <button
                 className={styles.buttonMenu2}
@@ -91,9 +91,9 @@ const ParametersPge = () => {
                   alt=""
                   src="/menu-square.svg"
                 />
-                <div className={styles.account2}>Change Hyper-Parameters</div>
+                <div className={styles.account}>Change Hyper-Parameters</div>
                 <img
-                  className={styles.menuSquareIcon1}
+                  className={styles.mediaCirclesPlayCircles1}
                   alt=""
                   src="/menu-square.svg"
                 />
@@ -103,10 +103,18 @@ const ParametersPge = () => {
                 className={styles.buttonMenu3}
                 onClick={onButtonMenu3Click}
               >
-                <img className={styles.imageIcon} alt="" src="/image.svg" />
-                <div className={styles.account3}>Results</div>
-                <img className={styles.imageIcon1} alt="" src="/image.svg" />
-                <div className={styles.glow4} />
+                <img
+                  className={styles.mediaCirclesPlayCircles}
+                  alt=""
+                  src="/image.svg"
+                />
+                <div className={styles.account}>Results</div>
+                <img
+                  className={styles.handPointFinger1}
+                  alt=""
+                  src="/image.svg"
+                />
+                <div className={styles.glow1} />
               </button>
             </div>
           </div>
@@ -118,23 +126,27 @@ const ParametersPge = () => {
             </div>
             <button className={styles.buttonMenu4}>
               <img
-                className={styles.settings2Icon}
+                className={styles.mediaCirclesPlayCircles}
                 alt=""
                 src="/settings-2.svg"
               />
-              <div className={styles.account4}>Settings</div>
+              <div className={styles.account}>Settings</div>
               <img
-                className={styles.settings2Icon1}
+                className={styles.handPointFinger1}
                 alt=""
                 src="/settings-2.svg"
               />
-              <div className={styles.glow6} />
+              <div className={styles.glow1} />
             </button>
             <button className={styles.buttonMenu5}>
-              <img className={styles.helpIcon} alt="" src="/help.svg" />
-              <div className={styles.account5}>Help center</div>
-              <img className={styles.helpIcon1} alt="" src="/help.svg" />
-              <div className={styles.glow7} />
+              <img
+                className={styles.mediaCirclesPlayCircles}
+                alt=""
+                src="/help.svg"
+              />
+              <div className={styles.account}>Help center</div>
+              <img className={styles.handPointFinger1} alt="" src="/help.svg" />
+              <div className={styles.glow1} />
             </button>
           </div>
         </div>
@@ -173,20 +185,20 @@ const ParametersPge = () => {
           </div>
         </div>
         <div className={styles.framesPerBatch}>
-          <div className={styles.frame1} />
+          <div className={styles.frame} />
           <input
-            className={styles.typeInResponse1}
+            className={styles.typeInResponse}
             placeholder="Type your value here"
             type="text"
           />
-          <div className={styles.content1}>
-            <div className={styles.title1}>
-              <div className={styles.parameter1}>Total Frames</div>
+          <div className={styles.content}>
+            <div className={styles.title}>
+              <div className={styles.parameter}>Total Frames</div>
             </div>
-            <div className={styles.divider2}>
-              <div className={styles.line2} />
+            <div className={styles.divider1}>
+              <div className={styles.line1} />
             </div>
-            <div className={styles.descLoremIpsum1}>
+            <div className={styles.descLoremIpsum}>
               Total frames refers to the total number of observations or frames
               collected from the environment during the entire training process
               of a reinforcement learning algorithm. It represents the amount of
@@ -196,20 +208,20 @@ const ParametersPge = () => {
           </div>
         </div>
         <div className={styles.learningRate}>
-          <div className={styles.frame2} />
+          <div className={styles.frame} />
           <input
-            className={styles.typeInResponse2}
+            className={styles.typeInResponse}
             placeholder="Type your value here"
             type="text"
           />
           <div className={styles.content2}>
-            <div className={styles.title2}>
-              <div className={styles.parameter2}>Learning Rate</div>
+            <div className={styles.title}>
+              <div className={styles.parameter}>Learning Rate</div>
             </div>
-            <div className={styles.divider3}>
-              <div className={styles.line3} />
+            <div className={styles.divider1}>
+              <div className={styles.line1} />
             </div>
-            <div className={styles.descLoremIpsumContainer}>
+            <div className={styles.descLoremIpsum}>
               <p className={styles.theLearningRate}>
                 The learning rate in machine learning determines the step size
                 at which a model adjusts its parameters during training,
@@ -217,8 +229,8 @@ const ParametersPge = () => {
                 hyper-parameter that balances the trade-off between rapid
                 learning and overshooting optimal solutions.
               </p>
-              <p className={styles.blankLine}>&nbsp;</p>
-              <p className={styles.recommendedRangeIs}>
+              <p className={styles.theLearningRate}>&nbsp;</p>
+              <p className={styles.theLearningRate}>
                 Recommended range is between 0.00001 and 0.1
               </p>
             </div>
@@ -226,24 +238,24 @@ const ParametersPge = () => {
         </div>
         <div className={styles.storage}>
           <div className={styles.parameterbox1}>
-            <div className={styles.frame3} />
+            <div className={styles.frame} />
             <div className={styles.typeInResponse3}>
               <div className={styles.typeInResponseChild} />
               <div className={styles.typeYourAnswer}>Type your value here</div>
             </div>
-            <div className={styles.content3}>
-              <div className={styles.title3}>
-                <div className={styles.parameter3}>Storage</div>
+            <div className={styles.content2}>
+              <div className={styles.title}>
+                <div className={styles.parameter}>Storage</div>
               </div>
-              <div className={styles.divider4}>
-                <div className={styles.line4} />
+              <div className={styles.divider1}>
+                <div className={styles.line1} />
               </div>
-              <div className={styles.descLoremIpsumContainer1}>
+              <div className={styles.descLoremIpsum}>
                 <p
-                  className={styles.circularStorageStructures}
+                  className={styles.theLearningRate}
                 >{`Circular storage structures, such as circular buffers, optimize memory usage by reusing space efficiently, while deques (double-ended queues) offer versatility with fast insertion and deletion operations at both ends, making them suitable for a wide range of dynamic data storage needs. `}</p>
-                <p className={styles.blankLine1}>&nbsp;</p>
-                <p className={styles.choicesAreCircular}>
+                <p className={styles.theLearningRate}>&nbsp;</p>
+                <p className={styles.theLearningRate}>
                   Choices are Circular Buffer or Deque Memory
                 </p>
               </div>
@@ -254,25 +266,25 @@ const ParametersPge = () => {
             <div className={styles.choice1}>Deque Memory</div>
           </button>
           <button className={styles.choice2}>
-            <div className={styles.choiceItem} />
-            <div className={styles.choice3}>Circular Buffer</div>
+            <div className={styles.choiceChild} />
+            <div className={styles.choice1}>Circular Buffer</div>
           </button>
         </div>
         <div className={styles.activationFunctions}>
-          <div className={styles.frame4} />
+          <div className={styles.frame} />
           <div className={styles.typeInResponse4}>
-            <div className={styles.typeInResponseItem} />
-            <div className={styles.typeYourAnswer1}>Type your value here</div>
+            <div className={styles.typeInResponseChild} />
+            <div className={styles.typeYourAnswer}>Type your value here</div>
           </div>
-          <div className={styles.content4}>
-            <div className={styles.title4}>
-              <div className={styles.parameter4}>Activation Functions</div>
+          <div className={styles.content2}>
+            <div className={styles.title}>
+              <div className={styles.parameter}>Activation Functions</div>
             </div>
-            <div className={styles.divider5}>
-              <div className={styles.line5} />
+            <div className={styles.divider1}>
+              <div className={styles.line1} />
             </div>
-            <div className={styles.descLoremIpsumContainer2}>
-              <p className={styles.activationFunctionsIntroduce}>
+            <div className={styles.descLoremIpsum}>
+              <p className={styles.theLearningRate}>
                 {`Activation functions introduce non-linearity to neural networks. They enable neural networks to model complex relationships and capture nonlinear patterns within data, enhancing the network's ability to learn and make predictions. Find a comprehensive list `}
                 <a
                   className={styles.here}
@@ -284,46 +296,46 @@ const ParametersPge = () => {
                 . You need the umber of activation functions to be the number of
                 layers plus one.
               </p>
-              <p className={styles.exampleNnreluNnrelu}>
+              <p className={styles.theLearningRate}>
                 Example: [nn.ReLU, nn.ReLU, nn.SiLU, nn.Sigmoid]
               </p>
             </div>
           </div>
         </div>
         <div className={styles.nueronsPerLayer}>
-          <div className={styles.frame5} />
+          <div className={styles.frame} />
           <input
-            className={styles.typeInResponse5}
+            className={styles.typeInResponse}
             placeholder="Type your value here"
             type="text"
           />
-          <div className={styles.content5}>
-            <div className={styles.title5}>
-              <div className={styles.parameter5}>Neurons Per Layer</div>
+          <div className={styles.content2}>
+            <div className={styles.title}>
+              <div className={styles.parameter}>Neurons Per Layer</div>
             </div>
-            <div className={styles.divider6}>
-              <div className={styles.line6} />
+            <div className={styles.divider1}>
+              <div className={styles.line1} />
             </div>
             <div
-              className={styles.descLoremIpsum2}
+              className={styles.descLoremIpsum}
             >{`Typically, smaller networks with fewer neurons per layer are preferred to reduce computational overhead. As the complexity of the task grows, larger networks with more neurons per layer may be necessary to better represent the structure of the problem. You can input any number of neurons and layers. An example of two layers of 128 and a final layer of 64 would look like: [128, 128, 64] `}</div>
           </div>
         </div>
         <div className={styles.ppqVsDqn}>
-          <div className={styles.parameterbox11}>
-            <div className={styles.frame6} />
-            <div className={styles.typeInResponse6}>
-              <div className={styles.typeInResponseInner} />
-              <div className={styles.typeYourAnswer2}>Type your value here</div>
+          <div className={styles.parameterbox1}>
+            <div className={styles.frame} />
+            <div className={styles.typeInResponse3}>
+              <div className={styles.typeInResponseChild} />
+              <div className={styles.typeYourAnswer}>Type your value here</div>
             </div>
-            <div className={styles.content6}>
-              <div className={styles.title6}>
-                <div className={styles.parameter6}>PPQ vs DQN</div>
+            <div className={styles.content2}>
+              <div className={styles.title}>
+                <div className={styles.parameter}>PPQ vs DQN</div>
               </div>
-              <div className={styles.divider7}>
-                <div className={styles.line7} />
+              <div className={styles.divider1}>
+                <div className={styles.line1} />
               </div>
-              <div className={styles.descLoremIpsum3}>
+              <div className={styles.descLoremIpsum}>
                 PPO and DQN are reinforcement learning algorithms tailored for
                 different types of environments. PPO is well-suited for
                 continuous action spaces, prioritizing stability and ease of
@@ -335,12 +347,12 @@ const ParametersPge = () => {
             </div>
           </div>
           <button className={styles.choice4}>
-            <div className={styles.choiceInner} />
-            <div className={styles.choice5}>DQN</div>
+            <div className={styles.choiceChild} />
+            <div className={styles.choice1}>DQN</div>
           </button>
-          <button className={styles.choice6}>
-            <div className={styles.rectangleDiv} />
-            <div className={styles.choice7}>PPO</div>
+          <button className={styles.choice2}>
+            <div className={styles.choiceChild} />
+            <div className={styles.choice1}>PPO</div>
           </button>
         </div>
       </div>
